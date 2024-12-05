4f5dab14dca6f6ea5edf749fedac17ba5afbd5ca
Vladislav Tankov <foraxru@gmail.com>
2024-12-05 23:22:11 +0300

    change userController

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,18 +1,34 @@
 class UserController {
   async registration(req, res) {
+    try {
 
+    } catch(error) {
+
+    }
   }
 
   async login(req, res) {
+    try {
 
+    } catch(error) {
+
+    }
   }
 
   async logout(req, res) {
+    try {
 
+    } catch(error) {
+
+    }
   }
 
   async refresh(req, res) {
+    try {
 
+    } catch(error) {
+
+    }
   }
 }
 
