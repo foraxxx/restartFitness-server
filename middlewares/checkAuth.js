77f3d0e376cccfbf58ac2af36ba3508465bc4a53
Vladislav Tankov <foraxru@gmail.com>
2024-12-10 20:58:40 +0300

    fix checkauth

--- middlewares/checkAuth.js
+++ middlewares/checkAuth.js
@@ -6,8 +6,8 @@ export default function (req, res, next) {
     const authorizationHeader = req.headers.authorization
 
     if (!authorizationHeader) {
+      return next(ApiError.UnauthorizedError())
     }
-    return next(ApiError.UnauthorizedError())
 
     const accessToken = authorizationHeader.split(' ')[1]
 
