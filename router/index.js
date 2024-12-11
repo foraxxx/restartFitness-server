d23bbe4552f59127549b063d85543c530c6334a2
Vladislav Tankov <foraxru@gmail.com>
2024-12-11 13:14:27 +0300

    add memberships router

--- router/index.js
+++ router/index.js
@@ -1,10 +1,12 @@
 import Router from 'express'
 import userRouter from './userRouter.js'
 import roleRouter from './roleRouter.js'
+import membershipRouter from "./membershipRouter.js"
 
 const router = new Router()
 
 router.use('/user', userRouter)
 router.use('/role', roleRouter)
+router.use('/memberships', roleRouter)
 
 export default router
---
+++ router/membershipRouter.js
@@ -0,0 +1,11 @@
+import Router from 'express'
+import membershipController from "../controllers/membershipController.js"
+
+const router = Router()
+
+router.get('/', membershipController.getAll)
+router.get('/:id', membershipController.getOne)
+router.put('/:id', membershipController.updateOne)
+router.delete('/:id', membershipController.deleteOne)
+
+export default router
