c5b38d319b49d0b04f9099fe65a452c97ff0d9ce
Vladislav Tankov <foraxru@gmail.com>
2024-12-11 15:42:03 +0300

    add path in index

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ import router from "./router/index.js"
 import cookieParser from "cookie-parser"
 import fileUpload from "express-fileupload"
 import errorsMiddleware from "./middlewares/errorsMiddleware.js"
+import path from "path"
 
 dotenv.config()
 const PORT = process.env.PORT || 4000
@@ -35,6 +36,7 @@ const app = express()
 app.use(cors())
 app.use(cookieParser())
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname, "static")))
 app.use(fileUpload({}))
 app.use('/api', router)
 app.use(errorsMiddleware)
