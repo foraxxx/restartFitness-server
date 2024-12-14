d884fcbae0024e7d56b56c2a16ab6667996363d7
Vladislav Tankov <foraxru@gmail.com>
2024-12-14 12:51:49 +0300

    add reviews router

--- router/index.js
+++ router/index.js
@@ -4,6 +4,7 @@ import roleRouter from './roleRouter.js'
 import membershipRouter from "./membershipRouter.js"
 import trainerRouter from "./trainerRouter.js"
 import userMembershipsRouter from "./userMembershipsRouter.js"
+import reviewsRouter from "./reviewsRouter.js"
 import checkAuth from "../middlewares/checkAuth.js"
 
 const router = new Router()
@@ -13,5 +14,6 @@ router.use('/role', roleRouter)
 router.use('/memberships', membershipRouter)
 router.use('/usermemberships', checkAuth, userMembershipsRouter)
 router.use('/trainers', trainerRouter)
+router.use('/reviews', reviewsRouter)
 
 export default router
