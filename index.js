863eb091c011c8533f84096bfff2ccdc6622b37e
Vladislav Tankov <foraxru@gmail.com>
2024-12-15 00:56:36 +0300

    add cors parameters

--- index.js
+++ index.js
@@ -33,7 +33,10 @@ dotenv.config()
 const PORT = process.env.PORT || 4000
 
 const app = express()
-app.use(cors())
+app.use(cors({
+  credentials: true,
+  origin:process.env.CLIENT_URL
+}))
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.static(path.resolve(process.cwd(), 'static')));
