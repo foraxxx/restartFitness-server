c0b5aa5ec84b77a13a48bfa7fe0a4523f53e926a
Vladislav Tankov <foraxru@gmail.com>
2025-06-22 23:25:34 +0300

    cors

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const app = express()
 // }))
 
 app.use(cors({
-  origin: process.env.CLIENT_URL,
+  origin: 'https://restart-fitness-client.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
