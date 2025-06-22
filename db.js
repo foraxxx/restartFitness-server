edd640d02ac2e195bee8bcd9ae9ce20033651d37
Vladislav Tankov <foraxru@gmail.com>
2025-06-22 22:37:13 +0300

    Изменил README.md

--- db.js
+++ db.js
@@ -2,15 +2,26 @@ import {Sequelize} from 'sequelize'
 import dotenv from "dotenv"
 dotenv.config()
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    dialect: 'postgres',
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-  }
-)
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     dialect: 'postgres',
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//   }
+// )
 
-export default sequelize
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,  // для Neon обязательно
+    },
+  },
+});
+
+export default sequelize;
